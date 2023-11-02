8118bf032c9769338bde36866f4ff44a43522940
Khayaeljuniormokoena <khayamokoena82@gmail.com>
2023-11-02 05:54:40 -0700

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if__name__ == "__main__":
+if __name__ == "__main__":
     import sys
     result = 0
+    
     for arg in sys.argv[1:]:
         result += int(arg)
-        print("{}".format(result))
+        print(result)
