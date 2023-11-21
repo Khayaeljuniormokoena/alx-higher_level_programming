5bcd3232d505d9ece854d82fdf655c0b93d9450f
Khayaeljuniormokoena <khayamokoena82@gmail.com>
2023-11-21 10:40:48 -0800

    update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,25 +1,49 @@
 #!/usr/bin/python3
-"""
-Class Square that defines a square with private instance attribute size
-and handles instantiation with optional size, type/ value verification,
-and includes properties for getting and setting size, along with
-a public instance method area to calculate the square area
-"""
+"""Defines a class Square"""
+
+
 class Square:
+    """
+    Class that defines properties of square by: (based on 3-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size=0):
-        self.size = size
+        """Creates new instances of square.
+
+        Args:
+            size: size of the square (1 side).
+        """
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of square.
+
+        Returns: the current square area.
+        """
+        return self.__size ** 2
 
     @property
     def size(self):
+        """Returns the size of a square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Property setter for size.
+
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        return self.__size ** 2
+        else:
+            self.__size = value
